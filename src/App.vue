2bbc3a01e280452da5574de3311d155a570fa90e
TuanDo <tuandd.gplaystudio@gmail.com>
2024-09-23 23:44:12 +0700

    init base

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
 </script>
 
 <template>
-
+  <div>
+    <div class="w-1/3">
+      sidebar
+    </div>
+    <div class="w-2/3">
+      content
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/index.css'
 
 const app = createApp(App)
 
